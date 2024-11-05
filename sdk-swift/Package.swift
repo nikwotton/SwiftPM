104913d4eee94131f57cd50b11bdfed0ef4537b3
Nikolai Wotton <wottonn@gmail.com>
2024-11-05 12:03:47 -0600

    naming

--- sdk-swift/Package.swift
+++ sdk-swift/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "sdk-swift",
-            path: "./sdk-swift.xcframework"
+            path: "./Candle.xcframework"
         )
     ]
 )
