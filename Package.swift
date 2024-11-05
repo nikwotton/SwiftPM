e45f28257c7fd4bfa042620df03e607db66ab8a1
Nikolai Wotton <wottonn@gmail.com>
2024-11-05 11:11:24 -0600

    adding first binary

--- Package.swift
+++ Package.swift
@@ -20,5 +20,10 @@ let package = Package(
             name: "sdk-swiftTests",
             dependencies: ["sdk-swift"]
         ),
+        .binaryTarget(
+            name: "binaryCandle",
+            url: "https://github.com/candlefinance/sdk-swift/blob/main/Candle.zip",
+            checksum: "e67e680ad128b9774da276a17768dfd501335f08f4ea4a12bf1830c1b7b114f5"
+        ),
     ]
 )
