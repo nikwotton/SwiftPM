f584be950f224aa1a130fca9c8493ee82b6b1a80
Nikolai Wotton <wottonn@gmail.com>
2024-11-05 11:43:17 -0600

    different folder structure

--- CandleSDK/Package.swift
+++ CandleSDK/Package.swift
@@ -1,10 +1,9 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "sdk-swift",
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -14,16 +13,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "sdk-swift"),
-        .testTarget(
-            name: "sdk-swiftTests",
-            dependencies: ["sdk-swift"]
-        ),
         .binaryTarget(
-            name: "binaryCandle",
-            url: "https://github.com/candlefinance/sdk-swift/blob/main/Candle.zip",
-            checksum: "e67e680ad128b9774da276a17768dfd501335f08f4ea4a12bf1830c1b7b114f5"
-        ),
+            name: "sdk-swift",
+            path: "./Candle.xcframework"
+        )
     ]
 )
