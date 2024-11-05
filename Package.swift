700384baf2c90593b4d25df1d75c6bd4cf9ef2e9
Nikolai Wotton <wottonn@gmail.com>
2024-11-05 17:06:12 -0600

    new framework

--- Package.swift
+++ Package.swift
@@ -2,20 +2,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "sdk-swift",
-    platforms: [.iOS(.v13), .macOS(.v10_15)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "sdk-swift",
-            targets: ["sdk-swift"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(
-            name: "sdk-swift",
-            path: "./sdk-swift.xcframework"
-        )
-    ]
+  name: "SDKSwift123",
+  platforms: [.iOS(.v13), .macOS(.v10_15)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "SDKSwift123",
+      targets: ["SDKSwift123"])
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .binaryTarget(
+      name: "SDKSwift123",
+      path: "./SDKSwift123.xcframework"
+    )
+  ]
 )
